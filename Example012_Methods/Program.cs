ac997e14cf8f59a3e5370cc95a8d2c760c9f7540
grezli <grezli@mail.ru>
2022-07-12 15:12:26 +0300

    Example 012 Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -150,12 +150,13 @@ void SelectionSort(int[] array)
 {
     for (int i = 0; i < array.Length - 1; i++)
     {
-        int minPosition = i;                  
-
-        for (int j = i + 1; j < array.Length; j++)
+        int minPosition = i;                      // minPosition =7
+        for(int j = i + 1; j < array.Length; j++)
         {
             if (array[j] < array[minPosition])
+            {
             minPosition = j;
+            }
         }
                                               //           [i]  0 1 2 3 4 5 6 7 8  
         int temporary = array[i];             // temp=array[7] {1,5,4,3,2,6,7,1,1}
